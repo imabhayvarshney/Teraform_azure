6b534325a2b688ae01c13b74703b2bc39706a899
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-09-18 13:47:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -48,7 +48,31 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
-resource "azurerm_virtual_machine" "main" {
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.main.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+/*resource "azurerm_virtual_machine" "main" {
     name                  = "${var.prefix}-vm"
     location              = azurerm_resource_group.example.location
     resource_group_name   = azurerm_resource_group.example.name
@@ -65,5 +89,5 @@ resource "azurerm_virtual_machine" "main" {
       computer_name  = "hostname"
       admin_username = "testadmin"
       admin_password = "Password1234!"
-    }
-}
+    	}
+    }*/
