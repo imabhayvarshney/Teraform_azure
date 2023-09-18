556e983be6ec94f39c6e28816f124160bbd477e9
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-09-18 13:23:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,69 +1,69 @@
 terraform {
-    required_providers {
-        azurerm  = {
-            source = "hashicorp/azurerm"
-            version = "~>2/88/1"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
     }
-    required_version = ">=0.15"
-}
-provider "azurerm"{
-    features {}
+  }
 }
 
-resource "azurerm_rg" "main" {
-    name = ""
-    location = ""
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
 }
 
-resource "azurerm_vnet" "main"{
-    name = "vnet1"
-    location = azurerm_rg.main.location
-    rg_name = azurerm_rg.main.name 
-    address_space = ["10.0.0.0/16"]
+variable "prefix" {
+  default = "tfvmex"
 }
 
-resource "azurerm_subnet" "main" {
-    name = "learn-subnet"
-    vnet_name = azurerm_vnet.main.name 
-    sg_name = azurerm_rg.main.name
-    address_prefix = ["10.0.0.0/24"]
+resource "azurerm_resource_group" "example" {
+    name = "1-1cb6034c-playground-sandbox"
+    location = "South Central US"
 }
 
-resource "azurerm_nic" "internal" {
-    name = "learn-nic"
-    location = azurerm_rg.main.location
-    rg_name = azurerm_rg.main.name
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
 
-    ip_configuration {
-        name = "internal"
-        subnet_id = azurerm_subnet.main.id
-        private_ip_address_allocation = "Dynamic"
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+    name                  = "${var.prefix}-vm"
+    location              = azurerm_resource_group.example.location
+    resource_group_name   = azurerm_resource_group.example.name
+    network_interface_ids = [azurerm_network_interface.main.id]
+    vm_size               = "Standard_DS1_v2"
+
+    storage_os_disk {
+      name              = "myosdisk1"
+      caching           = "ReadWrite"
+      create_option     = "FromImage"
+      managed_disk_type = "Standard_LRS"
     }
-}
-
-resource "azurerm_vm" "main"{
-    name = "learn-vm"
-    rg_name = azurerm_rg.main.name
-    location = azurerm_rg.main.location
-    size = "Standard_B1s"
-    admin_username = "user.admin"
-    admin_password = "Demopass12"
-
-    network_interface_ids = [
-        azurerm_nic.internal.id
-    ]
-    os_disk {
-        caching = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
-
-    source_image_refrence {
-        publisher = "MicrosftwindowsServer"
-        offer = "WindowsServer"
-        sku = "2016-DataCenter"
-        version = "latest"
-
+    os_profile {
+      computer_name  = "hostname"
+      admin_username = "testadmin"
+      admin_password = "Password1234!"
     }
 }
