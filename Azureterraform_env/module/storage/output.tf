6bab95dc08e3e56287eb006d39704889c1cc9ea8
imabhayvarshney <80506476+imabhayvarshney@users.noreply.github.com>
2023-09-22 20:48:36 +0530

    Delete Azureterraform_env directory

--- Azureterraform_env/module/storage/output.tf
+++
@@ -1,9 +0,0 @@
-output "storageaccountname" {
-  description = "ID of the virtual network."
-  value       = azurerm_storage_account. mystorageaccount.name
-}
-
-output "containername" {
-  description = "ID of the virtual network."
-  value       = azurerm_storage_container.mystoragecontainer.name
-}
